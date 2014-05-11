f550cf6d8844bc181e84eabca76e5d862eca353b
Alex <alexander.orlov@loxal.net>
2014-05-11 18:55:08 +0200

    cleanup slate

--- src/test/java/net/loxal/example/ExampleTest.java
+++ src/test/java/net/loxal/example/ExampleTest.java
@@ -4,8 +4,6 @@
 
 package net.loxal.example;
 
-import org.apache.commons.lang3.ArrayUtils;
-import org.apache.commons.lang3.RandomStringUtils;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.BeforeClass;
@@ -13,56 +11,13 @@ import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.lang.reflect.Method;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.LinkedList;
-import java.util.List;
-
-import static org.junit.Assert.assertArrayEquals;
-import static org.junit.Assert.assertEquals;
-
 public class ExampleTest {
     private static final Logger LOG = LoggerFactory.getLogger(ExampleTest.class);
-    static List<Character> unsortedCharacters;
-    static List<Character> sortedCharacters;
     private final Example example = new Example();
-
-    private static List<Character> createCharacters() {
-        char[] randomChars = RandomStringUtils.random(1234, "abcdefghijklmnopqrstuvwxyz").toCharArray();
-        List<Character> randomCharacters = Arrays.asList(ArrayUtils.toObject(randomChars));
-        List<Character> characters = new LinkedList<>();
-        characters.add('z');
-        characters.add('z');
-        characters.add('z');
-        characters.add('b');
-        characters.add('a');
-        characters.add('c');
-        characters.add('y');
-        characters.add('c');
-        characters.addAll(randomCharacters);
-        characters.add('d');
-        characters.add('x');
-        characters.add('f');
-        characters.add('e');
-        characters.add('e');
-        characters.add('e');
-        characters.add('u');
-        characters.add('v');
-        characters.add('u');
-        characters.add('a');
-        characters.add('a');
-
-        return characters;
-    }
 
     @BeforeClass
     public static void beforeClass() {
         LOG.info("@BeforeClass");
-        unsortedCharacters = createCharacters();
-
-        sortedCharacters = new LinkedList<>(unsortedCharacters);
-        Collections.sort(sortedCharacters);
     }
 
     @Before
@@ -75,25 +30,7 @@ public class ExampleTest {
     }
 
     @Test
-    public void sortUsingBubleSort() throws Exception {
-        Method sort = Example.class.getDeclaredMethod("sortUsingBubbleSort", List.class);
-        Character[] characters = (Character[]) sort.invoke(example, unsortedCharacters);
+    public void someTest() throws Exception {
 
-        verifySortOrder(characters);
     }
-
-    private void verifySortOrder(Character[] characters) {
-        assertEquals(sortedCharacters, Arrays.asList(characters));
-        assertArrayEquals(sortedCharacters.toArray(), Arrays.asList(characters).toArray());
-    }
-
-    @Test
-    public void sortUsingImprovedBubleSort() throws Exception {
-        Example example = new Example();
-        Method sort = Example.class.getDeclaredMethod("sortUsingImprovedBubbleSort", List.class);
-        Character[] characters = (Character[]) sort.invoke(example, unsortedCharacters);
-
-        verifySortOrder(characters);
-    }
-
 }
