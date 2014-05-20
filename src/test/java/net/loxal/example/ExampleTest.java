710904fe7bc15ce92ed4c8edce3c97dcdd8320a3
Alex <alexander.orlov@loxal.net>
2014-05-20 15:37:58 +0200

    NPE does not prevent @After and @AfterClass from being executed in JUnit

--- src/test/java/net/loxal/example/ExampleTest.java
+++ src/test/java/net/loxal/example/ExampleTest.java
@@ -5,6 +5,7 @@
 package net.loxal.example;
 
 import org.junit.After;
+import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
@@ -13,24 +14,32 @@ import org.slf4j.LoggerFactory;
 
 public class ExampleTest {
     private static final Logger LOG = LoggerFactory.getLogger(ExampleTest.class);
-    private final Example example = new Example();
+    private Example example;
+    private Object o;
 
     @BeforeClass
     public static void beforeClass() {
         LOG.info("@BeforeClass");
     }
 
+    @AfterClass
+    public static void afterClass() throws Exception {
+        LOG.info("@AfterClass");
+    }
+
     @Before
     public void setUp() throws Exception {
         LOG.info("@Before");
+        example = new Example();
     }
 
     @After
     public void tearDown() throws Exception {
+        LOG.info("@After");
     }
 
     @Test
     public void someTest() throws Exception {
-
+        throw new NullPointerException("NPE");
     }
 }
