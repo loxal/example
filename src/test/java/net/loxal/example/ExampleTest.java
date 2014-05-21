b047b32cdaeb5fac34b069d5c5dc875012ee742a
Alex <alexander.orlov@loxal.net>
2014-05-21 11:41:49 +0200

    In-Depth Understanding JUnit Tests blog post

--- src/test/java/net/loxal/example/ExampleTest.java
+++ src/test/java/net/loxal/example/ExampleTest.java
@@ -14,32 +14,44 @@ import org.slf4j.LoggerFactory;
 
 public class ExampleTest {
     private static final Logger LOG = LoggerFactory.getLogger(ExampleTest.class);
-    private Example example;
 
+    /**
+     * This method won't be executed if the unit test will be (externally) terminated (e.g. from within the IDE).
+     * You have to act **really** fast in this case.
+     */
     @BeforeClass
-    public static void beforeClass() {
-        LOG.info("@BeforeClass");
+    public static void beforeClass() throws Exception {
+        LOG.info("@BeforeClass"); // will be always executed, even if a test fails throwing any Exception
     }
 
+    /**
+     * This method won't be executed if the unit test will be (externally) terminated (e.g. from within the IDE).
+     */
     @AfterClass
     public static void afterClass() throws Exception {
-        LOG.info("@AfterClass");
+        LOG.info("@AfterClass"); // will be always executed, even if a test fails throwing any Exception
     }
 
+    /**
+     * This method won't be executed if the unit test will be (externally) terminated (e.g. from within the IDE).
+     * You have to act *really* fast in this case.
+     */
     @Before
     public void setUp() throws Exception {
-        LOG.info("@Before");
-        example = new Example();
+        LOG.info("@Before"); // will be always executed, even if a test fails throwing any Exception
     }
 
+    /**
+     * This method won't be executed if the unit test will be (externally) terminated (e.g. from within the IDE).
+     */
     @After
     public void tearDown() throws Exception {
-        LOG.info("@After");
+        LOG.info("@After"); // will be always executed, even if a test fails throwing any Exception
     }
 
     @Test
     public void someTest() throws Exception {
-        Thread.sleep(10000);
-        throw new Error("VM Error");
+        Thread.sleep(10000); // interrupts current "main" Thread in the "main" thread group
+        throw new Error("VM Error"); // allowed as Exception is part of this test method's signature ("throws" clause)
     }
 }
