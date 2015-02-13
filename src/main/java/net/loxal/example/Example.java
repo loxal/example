ba46b46247ffe26309f78944d8135c66a73cfb3f
Alex <alexander.orlov@loxal.net>
2015-02-13 15:34:21 +0100

    evolve

--- src/main/java/net/loxal/example/Example.java
+++ src/main/java/net/loxal/example/Example.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2014 Alexander Orlov <alexander.orlov@loxal.net>. All rights reserved.
+ * Copyright 2015 Alexander Orlov <alexander.orlov@loxal.net>. All rights reserved.
  */
 
 package net.loxal.example;
@@ -10,5 +10,9 @@ public class Example {
     public static final Logger LOG = Logger.getGlobal();
 
     public Example() {
+    }
+
+    public static void main(String... args) {
+        new Example();
     }
 }
